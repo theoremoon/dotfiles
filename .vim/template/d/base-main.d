ad28d84aadf7ecc7925dc8783e14cd9e6c9bf3f1
theoremoon <theoldmoon0602@gmail.com>
2020-01-12 20:55:14 +0900

    add numeric

--- .vim/template/d/base-main.d
+++ .vim/template/d/base-main.d
@@ -1,4 +1,4 @@
-import std.stdio, std.string, std.algorithm, std.array, std.range, std.conv, std.typecons, std.math, std.container, std.format;
+import std.stdio, std.string, std.algorithm, std.array, std.range, std.conv, std.typecons, std.math, std.container, std.format, std.numeric;
 
 void main(string[] args) {
 	{{_cursor_}}
