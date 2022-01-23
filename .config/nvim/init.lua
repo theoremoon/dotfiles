0c5f19b5489fc70062fd17f09c9a86332fc3cc69
theoremoon <theoldmoon0602@gmail.com>
2022-01-23 16:36:31 +0900

    tabstop,shiftwidth

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,9 @@ vim.o.signcolumn = 'yes'
 
 vim.o.mouse = 'a'
 vim.o.breakindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.ignorecase = true
 vim.o.updatetime = 200
 vim.o.splitright = true
