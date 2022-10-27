875649b634d309dfc4b19b3b895fc918376a283b
theoremoon <theoldmoon0602@gmail.com>
2022-10-27 09:37:42 +0900

    update_capabilities -> default_capabilities

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -284,7 +284,7 @@ cmp.setup {
   },
 }
 vim.api.nvim_set_keymap('i', '<C-x><C-o>', [[<Cmd>lua require('cmp').complete()<CR>]], { noremap = true, silent = true })
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- lsp
 vim.lsp.set_log_level(vim.log.levels.ERROR)
